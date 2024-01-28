392d1fb553ba21d9b19da6999b4c7a4d82d0bd55
michael youssef <michaelyoussef396@gmail.com>
2024-01-28 21:33:25 +1100

    added text and icons for hero in shop

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,9 +1,23 @@
 import React from "react";
 import '../Hero/Hero.css'
-
+import hand_icon from '../Assets/hand_icon.png'
 const Hero = () => {
     return (
+        <div className="hero">
+            <h2>NEW ARRIVALS ONLY</h2>
+            <div className="hero-left">
+                <div className="hand-hand-icon">
+                    <p>new</p>
+                    <img src={hand_icon} alt=""/>
+                </div>
+                <p>collections</p>
+                <p>for everyone</p>
+            </div>
 
+            <div className="hero-right">
+
+            </div>
+        </div>
     )
 }
 
