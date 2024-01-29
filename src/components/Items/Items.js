46462ecc0aed15213b5cf3e9de6da686895dcd0c
michael youssef <michaelyoussef396@gmail.com>
2024-01-29 20:12:11 +1100

    added css styling and sample product

--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -1,21 +1,21 @@
 import React from "react";
-import '../Items/Item.css'
+import '../Items/Item.css';
 
 const Item = (props) => {
     return (
         <div className="item">
-            <img src={props.image} alt="" />
+            <img src={props.image} alt={props.name} />
             <p>{props.name}</p>
             <div className="item-prices">
                 <div className="item-price-new">
-                    {props.new_price}
+                    ${props.new_price}
                 </div>
                 <div className="item-price-old">
-                    {price.old_price}
+                    ${props.old_price}
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Item;
--- src/components/Popular/Popular.js
+++ src/components/Popular/Popular.js
@@ -1,12 +1,18 @@
 import React from "react";
 import '../Popular/popular.css'
+import sample_popular_data from "../Assets/sample_data";
+import Item from "../Items/Items";
+
 const Popular = () => {
     return (
         <div className="popular">
             <h1>POPULAR IN WOMEN</h1>
             <hr />
             <div className="popular-item">
-                {/* add the popularitems here when teh server is running*/}
+                {/* add the popularitems from backend here when teh server is running*/}
+                {sample_popular_data.map((item, i)=>{
+                    return <Item key={i} id={item.id} name={item.name}  image={item.image} new_price={item.new_price} old_price={item.old_price} />
+                })}
             </div>
         </div>
     )
