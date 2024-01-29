f0d8f4e7de6b4226f33bf5c65d18cb81f24cea83
michael youssef <michaelyoussef396@gmail.com>
2024-01-29 19:05:31 +1100

    made popular component

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "../components/Hero/Hero";
+import Popular from "../components/Popular/Popular";
 
 
 const Shop = () => {
     return (
         <div>
             <Hero />
+            <Popular /> 
         </div>
     );
 }
