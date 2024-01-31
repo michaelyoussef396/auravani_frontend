3e68036bb3bc4ca7592541dc9d56ff78380522e8
michael youssef <michaelyoussef396@gmail.com>
2024-01-31 23:40:37 +1100

    added login page

--- src/pages/LoginSiqnup.js
+++ src/pages/LoginSiqnup.js
@@ -2,8 +2,21 @@ import React from "react";
 
 const LoginSignup = () => {
     return (
-        <div>
-
+        <div className="loginsignup">
+            <div className="loginsignup-container">
+                <h1>Sign Up</h1>
+                <div className="loginsignup-fields">
+                    <input type="text" placeholder="Your Name" />
+                    <input type="email" placeholder="Email Address" />
+                    <input type="password" placeholder="Password" />
+                </div>
+                <button>Contuine</button>
+                <p className="loginsignup-login">Already have an accoount? <span>Login Here</span></p>
+                <div className="loginsignup-agree">
+                    <input type="checkbox" name="" id=""/>
+                    <p>By Contuining, i agree to the terms and use & privacy policy</p>
+                </div>
+            </div>
         </div>
     );
 }
