d7e70c691ab9dd65e112ef57840b351ca4e84727
michael <michaelyoussef396@gmail.com>
2024-01-31 12:47:41 +1100

    linked backend to fornt end

--- src/components/Popular/Popular.js
+++ src/components/Popular/Popular.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import '../Popular/popular.css'
-import sample_popular_data from "../Assets/sample_data";
 import Item from "../Items/Items";
 
 const Popular = () => {
+    const [popularItems, setPopularItems] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/auravani_popular')
+            .then(r => r.json())
+            .then(data => setPopularItems(data))
+    })
+
     return (
         <div className="popular">
             <h1>POPULAR IN WOMEN</h1>
             <hr />
             <div className="popular-item">
-                {/* add the popularitems from backend here when teh server is running*/}
-                {sample_popular_data.map((item, i)=>{
-                    return <Item key={i} id={item.id} name={item.name}  image={item.image} new_price={item.new_price} old_price={item.old_price} />
+                {popularItems.map((item, i) => {
+                    return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
                 })}
             </div>
         </div>
