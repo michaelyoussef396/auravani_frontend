8be876111fdb232e2b7a55655528e56c1d3efba8
michael youssef <michaelyoussef396@gmail.com>
2024-01-31 13:39:22 +1100

    added newsletter componet

---
+++ src/components/NewsLetter/NewsLetter.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../NewsLetter/NewsLetter.css'
+
+const NewsLetter = () => {
+    return (
+        <div className="newsletter">
+            <h1>Get Exclisive Ofeers on your Email</h1>
+            <p>Subscribe to our newsletter and stay updated</p>
+            <div>
+                <input type='text' placeholder='Your Email' />
+                <button>Subscribe</button>
+            </div>
+        </div>
+    )
+}
